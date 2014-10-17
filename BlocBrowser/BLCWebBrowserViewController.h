e2168888991efb4bc6dac67a870aedb336ced450
Peter Gow <peter.gow@gmail.com>
2014-10-18 01:34:26 +1100

    Functional web browser

---
+++ BlocBrowser/BLCWebBrowserViewController.h
@@ -0,0 +1,13 @@
+//
+//  BLCWebBrowserViewController.h
+//  BlocBrowser
+//
+//  Created by Peter Gow on 16/10/2014.
+//  Copyright (c) 2014 Peter Gow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCWebBrowserViewController : UIViewController
+
+@end
